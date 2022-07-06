a10d083c43b1b6509006b4da2366f008d50f4645
potato <2789222473@qq.com>
2022-07-06 18:22:58 +0800

    delete unuse argment

--- zColor/color.go
+++ zColor/color.go
@@ -5,67 +5,67 @@ import (
 	"strings"
 )
 
-func Green(str string, modifier ...interface{}) string {
+func Green(str string) string {
 	return cliColorRender(str, 32, 0)
 }
 
-func LightGreen(str string, modifier ...interface{}) string {
+func LightGreen(str string) string {
 	return cliColorRender(str, 32, 1)
 }
 
-func Cyan(str string, modifier ...interface{}) string {
+func Cyan(str string) string {
 	return cliColorRender(str, 36, 0)
 }
 
-func LightCyan(str string, modifier ...interface{}) string {
+func LightCyan(str string) string {
 	return cliColorRender(str, 36, 1)
 }
 
-func Red(str string, modifier ...interface{}) string {
+func Red(str string) string {
 	return cliColorRender(str, 31, 0)
 }
 
-func LightRed(str string, modifier ...interface{}) string {
+func LightRed(str string) string {
 	return cliColorRender(str, 31, 1)
 }
 
-func Yellow(str string, modifier ...interface{}) string {
+func Yellow(str string) string {
 	return cliColorRender(str, 33, 0)
 }
 
-func Black(str string, modifier ...interface{}) string {
+func Black(str string) string {
 	return cliColorRender(str, 30, 0)
 }
 
-func DarkGray(str string, modifier ...interface{}) string {
+func DarkGray(str string) string {
 	return cliColorRender(str, 30, 1)
 }
 
-func LightGray(str string, modifier ...interface{}) string {
+func LightGray(str string) string {
 	return cliColorRender(str, 37, 0)
 }
 
-func White(str string, modifier ...interface{}) string {
+func White(str string) string {
 	return cliColorRender(str, 37, 1)
 }
 
-func Blue(str string, modifier ...interface{}) string {
+func Blue(str string) string {
 	return cliColorRender(str, 34, 0)
 }
 
-func LightBlue(str string, modifier ...interface{}) string {
+func LightBlue(str string) string {
 	return cliColorRender(str, 34, 1)
 }
 
-func Purple(str string, modifier ...interface{}) string {
+func Purple(str string) string {
 	return cliColorRender(str, 35, 0)
 }
 
-func LightPurple(str string, modifier ...interface{}) string {
+func LightPurple(str string) string {
 	return cliColorRender(str, 35, 1)
 }
 
-func Brown(str string, modifier ...interface{}) string {
+func Brown(str string) string {
 	return cliColorRender(str, 33, 0)
 }
 
